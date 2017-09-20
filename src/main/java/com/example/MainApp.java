8a98148cbcd798b152234a0f90f58c154af1163f
yasuaki9973 <estpolis1127@gmail.com>
2017-09-20 21:55:11 +0900

    不要な処理を削除

--- src/main/java/com/example/MainApp.java
+++ src/main/java/com/example/MainApp.java
@@ -2,7 +2,6 @@ package com.example;
 
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,12 +20,6 @@ public class MainApp implements CommandLineRunner {
         customerService.create(new Customer(null, "Nobita", "Nobi"));
         customerService.create(new Customer(null, "Takeshi", "Goda"));
         customerService.create(new Customer(null, "Shizuka", "Minamoto"));
-
-        List<Customer> list = customerService.findAll();
-
-        for (Customer customer : list) {
-            System.out.println(customer.getFirstName());
-        }
 
     }
 
