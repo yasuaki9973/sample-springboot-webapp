b748a222dfb05bab54f5868b052ad6f846f29355
yasuaki9973 <estpolis1127@gmail.com>
2017-10-06 23:33:41 +0900

    細かい修正

--- src/main/java/com/example/MainApp.java
+++ src/main/java/com/example/MainApp.java
@@ -1,34 +1,14 @@
 package com.example;
 
-import com.example.domain.Customer;
-import com.example.domain.User;
-import com.example.service.CustomerService;
-import com.example.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @SpringBootApplication
 public class MainApp implements CommandLineRunner {
 
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    CustomerService customerService;
-
     @Override
     public void run(String... strings) throws Exception {
-
-        String encodedPassword = new Pbkdf2PasswordEncoder().encode("root");
-        userService.create(new User("root", encodedPassword));
-
-        customerService.create(new Customer(null, "Hidetoshi", "Dekisugi", null), "root");
-        customerService.create(new Customer(null, "Nobita", "Nobi", null), "root");
-        customerService.create(new Customer(null, "Takeshi", "Goda", null), "root");
-        customerService.create(new Customer(null, "Shizuka", "Minamoto", null), "root");
 
     }
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_name, encoded_password) VALUES ('root', '8da305135513eacf201f57cb08240e525f199b4a878b87dd385df2fcbebbf4cc7cefdd10aa7d3eea');
+
+INSERT INTO customers (id, first_name, last_name, user_name) VALUES (1, 'Hidetoshi', 'Dekisugi', 'root');
+INSERT INTO customers (id, first_name, last_name, user_name) VALUES (2, 'Nobita', 'Nobi', 'root');
+INSERT INTO customers (id, first_name, last_name, user_name) VALUES (3, 'Takeshi', 'Goda', 'root');
+INSERT INTO customers (id, first_name, last_name, user_name) VALUES (4, 'Shizuka', 'Minamoto', 'root');
